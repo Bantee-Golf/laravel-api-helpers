597b0e24c8981da82cf02ba324a1bc9566d6490d
Shane Perera <shane@shaneperera.com>
2016-04-15 13:17:55 +1000

    Add validation failed modifier

---
+++ src/ModifyValidationFailedApiResponse.php
@@ -0,0 +1,27 @@
+<?php
+namespace EMedia\Api;
+
+use Illuminate\Http\Request;
+
+trait ModifyValidationFailedAPIResponse
+{
+	/**
+	 * Create the response for when a request fails validation.
+	 *
+	 * @param  \Illuminate\Http\Request  $request
+	 * @param  array  $errors
+	 * @return \Illuminate\Http\Response
+	 */
+	protected function buildFailedValidationResponse(Request $request, array $errors)
+	{
+		if (($request->ajax() && ! $request->pjax()) || $request->wantsJson()) {
+			$errors = array_flatten($errors);
+			$errors = implode(' ', $errors);
+			return response()->apiError($errors, 422);
+		}
+
+		return redirect()->to($this->getRedirectUrl())
+						 ->withInput($request->input())
+						 ->withErrors($errors, $this->errorBag());
+	}
+}
