cd0b9990059c9aef4dce3571240f2461757af7d0
Shane Perera <shane@shaneperera.com>
2017-04-16 02:18:15 +1000

    Update for L5.4

--- src/ModifyValidationFailedApiResponse.php
+++ src/ModifyValidationFailedApiResponse.php
@@ -17,7 +17,7 @@ trait ModifyValidationFailedAPIResponse
 		if (($request->ajax() && ! $request->pjax()) || $request->wantsJson()) {
 			$errors = array_flatten($errors);
 			$errors = implode(' ', $errors);
-			return response()->apiError($errors, 422);
+			return response()->apiError($errors, 422);	// HTTP_UNPROCESSABLE_ENTITY
 		}
 
 		return redirect()->to($this->getRedirectUrl())
